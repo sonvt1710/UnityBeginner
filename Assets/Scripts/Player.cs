89c008236d7beabae1ac8d7c8dadb51b0a244a8d
Son Vu <sonvt1710@gmail.com>
2023-10-11 09:07:21 +0700

    F**king first commit

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,29 @@
+using System.Collections.Generic;
+using UnityEngine;
+public class Player : MonoBehaviour
+{
+    public float velocity = 2.4f;
+    public bool isDead = false;
+    private Rigidbody2D rigidbody;
+    public GameManager gameManager;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody2D>();
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0) || Input.GetKeyDown(KeyCode.Space))
+        {
+            rigidbody.velocity = Vector2.up * velocity;
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        isDead = true;
+        gameManager.GameOver();
+    }
+}
